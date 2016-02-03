174ce5bb32792907e8e5dc180a20403d489304f2
vagrant <vagrant@lighthouse-labs>
2016-02-03 23:26:07 +0000

    switched to tracking letter index

--- word_count.rb
+++ word_count.rb
@@ -1,15 +1,23 @@
 def count_letters (string)
-  letter_count = {}
-  string.split(' ').each do |word|
-    word.split('').each do |letter|
-      if letter_count[letter] == nil
-        letter_count[letter] = 1
-      else
-        letter_count[letter] += 1
-      end
-    end
+  # letter_count = {}
+  # string.split('').each_with_index do |letter, index|
+  #   next if letter == " "
+  #   letter_count[letter] = index + 1
+  # end
+  letter_count = Hash.new { |x,y| x[y] = []}
+  string.split('').each_with_index do |letter,index|
+    next if letter == " "
+    letter_count[letter] << index
   end
+  # string.each_with_index do |letter|
+  #   next if letter == " "
+  #   if letter_count[letter] == nil
+  #     letter_count[letter] = 1
+  #   else
+  #     letter_count[letter] = 
   letter_count
 end
 
-puts count_letters("I just did that...")
+string1 = ("I just did that...")
+
+puts count_letters(string1)
